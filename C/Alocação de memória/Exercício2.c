2bed6a50b6b18251ee4cd7e43d226ebf4552198d
QTelesGabriel <qteles.gabriel@gmail.com>
2024-09-19 16:15:20 -0300

    Delete C directory

--- C/Alocação de memória/Exercício2.c
+++
@@ -1,9 +0,0 @@
-/*Utilizando *malloc, escreva um programa que aloque dinamicamente um inteiro*/
-
-#include <stdio.h>
-#include <stdlib.h>
-
-int main() {
-    int *num = (int *)malloc(sizeof(int*));
-    return 0;   
-}
